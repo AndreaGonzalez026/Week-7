c78a2b7ee9da7a823998775f12fc862c3a661f9f
Andrea Alheli Gonzalez <11069744@uvu.edu>
2024-06-22 23:00:19 -0600

    Add files via upload

---
+++ FollowPlayerX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 2, -10); // Adjust the camera position relative to the plane
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Follow the plane maintaining the offset
+        transform.position = player.transform.position + offset;
+        // Keep the camera looking at the plane
+        transform.LookAt(player.transform);
+    }
+}
+
---
+++ PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f; // Plane's speed
+    [SerializeField] private float tiltSpeed = 30f; // Tilt speed
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the plane forward at a constant speed
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // Get the user's input for tilting the plane
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Tilt the plane up or down
+        transform.Rotate(Vector3.right * verticalInput * Time.deltaTime * tiltSpeed);
+    }
+}
+
+
